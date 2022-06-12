4b67e63d6ba6d2c1ed96e9a98fdbe79082dbe3d1
Adam Bourke <csadambourke@gmail.com>
2022-06-12 21:36:57 +1000

    Map all contacts as props

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import { useState } from "react"
 import "./styles.css";
 
 const App = () => {
+  const contacts = [
+    { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25},
+    { name: "Jason Long", email: "jason.long@notreal.com", age: 45},
+    { name: "Peter Pan", email: "peter.pan@notreal.com", age: 100}
+  ];
 
   return (
     <>
-    <ContactCard />
-    <ContactCard />
-    <ContactCard />
+      {contacts.map(contact => (
+        <ContactCard
+          avatar="https://via.placeholder.com/150"
+          name={contact.name}
+          email={contact.email}
+          age={contact.age}
+        />
+      ))}
     </>
   );
 };
 
-const ContactCard = () => {
+const ContactCard = props => {
   const [showAge, setShowAge] = useState(false);
+  const [contacts, setContacts] = useState([]);
 
   return (
     <div className="contact-card">
       <img src="https://via.placeholder.com/150" alt="profile" />
       <div className="user-details">
-        <p>Name: Jenny Han</p>
-        <p>Email: Jenny.Han@notreal.com</p>
-        {showAge === true ? <p>Age: 25</p> : null}
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        {showAge === true ? <p>Age: {props.age}</p> : null}
         <button onClick={() => setShowAge(!showAge)}>
           Toggle Age
         </button>

b317b99f5adeea53ca7d567a32c91d82b9243698
Adam Bourke <csadambourke@gmail.com>
2022-06-12 20:58:03 +1000

    Added onClick event to message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <h2>{message}</h2>
+      <h2 onClick={() => alert("You clicked the message!")}>{message}</h2>
     </div>
   )
 }

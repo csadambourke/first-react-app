34d096fe615f1c4f82f210e562f28ae536d004fb
Adam Bourke <csadambourke@gmail.com>
2022-06-12 21:22:25 +1000

    Added new state showAge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react"
 import "./styles.css";
 
 const App = () => {
@@ -6,18 +7,25 @@ const App = () => {
   return (
     <>
     <ContactCard />
+    <ContactCard />
+    <ContactCard />
     </>
   );
 };
 
 const ContactCard = () => {
+  const [showAge, setShowAge] = useState(false);
+
   return (
     <div className="contact-card">
       <img src="https://via.placeholder.com/150" alt="profile" />
       <div className="user-details">
         <p>Name: Jenny Han</p>
         <p>Email: Jenny.Han@notreal.com</p>
-        <p>Age: 25</p>
+        {showAge === true ? <p>Age: 25</p> : null}
+        <button onClick={() => setShowAge(!showAge)}>
+          Toggle Age
+        </button>
       </div>
     </div>
   );
